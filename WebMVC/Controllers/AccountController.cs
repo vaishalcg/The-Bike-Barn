10d0e9d5bcf1f6e52efc4298cbeecf71f0d3bba8
shrgunja <shreyas-ramdas.gunjal@capgemini.com>
2024-09-11 16:15:03 +0530

    Added Auth

--- WebMVC/Controllers/AccountController.cs
+++ WebMVC/Controllers/AccountController.cs
@@ -17,7 +17,7 @@ namespace WebMVC.Controllers
     {
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
-
+        ApplicationDbContext _db = new ApplicationDbContext();
         public AccountController()
         {
         }
@@ -139,6 +139,7 @@ namespace WebMVC.Controllers
         [AllowAnonymous]
         public ActionResult Register()
         {
+            ViewBag.Name = new SelectList(_db.Roles.ToList(),"Name","Name");
             return View();
         }
 
@@ -155,14 +156,16 @@ namespace WebMVC.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    await UserManager.AddToRoleAsync(user.Id, model.Name);
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
+                    
+                    ViewBag.Name = new SelectList(_db.Roles.ToList(), "Name", "Name");
                     return RedirectToAction("Index", "Home");
                 }
                 AddErrors(result);
