9557b27b4db756980c9dc10bc3e8655dca235359
shrgunja <shreyas-ramdas.gunjal@capgemini.com>
2024-09-12 15:37:27 +0530

    Final

--- WebMVC/Controllers/OrderMVCController.cs
+++ WebMVC/Controllers/OrderMVCController.cs
@@ -175,7 +175,7 @@ namespace WebMVC.Controllers
 
         #region Create GET
         // GET: OrderMVC/Create
-        [Authorize(Roles ="Staff")]
+        [Authorize(Roles ="Staff,Store,Admin")]
         public ActionResult Create()
         {
             return View();
@@ -184,7 +184,7 @@ namespace WebMVC.Controllers
 
         #region Create POST
         // POST: OrderMVC/Create
-        [Authorize(Roles = "Staff")]
+        [Authorize(Roles = "Staff,Store,Admin")]
         [HttpPost]
         public ActionResult Create(Order orderObj)
         {
